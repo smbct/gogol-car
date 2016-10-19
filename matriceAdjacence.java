d115ee0284124705573999a952154fe08fa3c842
Samuel BUCHET <E134848Z@INFO2B2pc04.ensinfo.sciences.univ-nantes.prive>
2016-10-19 16:45:42 +0200

    premier commit

---
+++ matriceAdjacence.java
@@ -0,0 +1,7 @@
+public class matriceAdjacence {
+
+    public static void main(String[] argc) {
+        System.out.println("hello graphe");
+    }
+
+}
---
+++ ville.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ville {
+
+    private ArrayList<String> places;
+    private ArrayList<ArrayList<String>> rues;
+
+    public void parser(String fichier) {
+        Scanner sc = new Scanner(new File(fichier));
+    }
+
+    public static void main(String[] argc) {
+        System.out.println("hello graphe");
+    }
+
+}
